60e83f31686a4f84f7e78336a64d17713c85508d
Jafar <egamovkhurshed29@gmail.com>
2020-05-27 20:57:03 +0500

    Added function view with category

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -15,7 +15,17 @@ namespace AlifAdminMiniMarketV2.Controllers
         }
         public IActionResult Index()
         {
+            ViewBag.Categories = _context.Categories.ToList();
             var li = _context.Products.OrderByDescending(p => p).Include(p => p.ProductCategory).ToList();
+            return View(li);
+        }
+        [HttpPost]
+        public IActionResult Index(string category)
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            var li = _context.Products.OrderByDescending(p => p).Include(p => p.ProductCategory).Where(p=> p.ProductCategory.Category == category).ToList();
+            if(category == "Все")
+                li = _context.Products.OrderByDescending(p=> p).Include(p=> p.ProductCategory).ToList();
             return View(li);
         }
         public IActionResult Add()
@@ -37,6 +47,28 @@ namespace AlifAdminMiniMarketV2.Controllers
         {
             var model = await _context.Products.SingleAsync(p=> p.Id == id);
             _context.Remove(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var model = _context.Products.Include(p=> p.ProductCategory).Single(p=> p.Id == id);
+            var li = _context.Categories.Where(p=> p.Id == model.ProductCategory.Id).ToList();
+            foreach (var x in _context.Categories.ToList())
+            {
+                li.Add(x);
+            }
+            ViewBag.Categories = li;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product model,int xid)
+        {
+            var lastmodel = _context.Products.Single(p=> p.Id == model.Id);
+            var Category = _context.Categories.Single(p=> p.Id == xid);
+            lastmodel.ProductName = model.ProductName;
+            lastmodel.Price = model.Price;
+            lastmodel.ProductCategory = Category;
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
